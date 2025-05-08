64c4edf1e79e4c80d4d47d3f9ae669eb23f39da4
tejas harshad shah <73342855+tejas36026@users.noreply.github.com>
2025-05-08 11:22:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import { YoutubeTranscript } from 'youtube-transcript';
+
+
 // In aiimageeditro/server.js
 app.get('/api/transcript', async (req, res) => {
     const transcript = await YoutubeTranscript.fetchTranscript('IG0J_ynkemI');
